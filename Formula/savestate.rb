10388bfe3ea2b9d1c66e30e262c338ada850a950
DB Hurley <dbhurley@me.com>
2026-02-09 12:12:26 -0500

    savestate 0.7.0

--- Formula/savestate.rb
+++ Formula/savestate.rb
@@ -1,8 +1,8 @@
 class Savestate < Formula
   desc "Time Machine for AI — encrypted backup, restore, and migration for AI agent state"
   homepage "https://savestate.dev"
-  url "https://registry.npmjs.org/@savestate/cli/-/cli-0.2.0.tgz"
-  sha256 "1279717edb7445d136237fd9136173c4b3a653066b2313b63c69e3f5e3b88ba5"
+  url "https://registry.npmjs.org/@savestate/cli/-/cli-0.7.0.tgz"
+  sha256 "c8a469849b45579bc7efffb9d739b1232f78af38993e49e2d90c2598c41ace33"
   license "MIT"
 
   depends_on "node"
